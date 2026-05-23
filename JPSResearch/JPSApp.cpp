bbec904cbc2c334e35bb9d97f06280b692fde79c
Daniel <daniel.adamov@student.howest.be>
2026-05-23 16:19:24 +0200

    Initial Commit
    Using self-made Game Engine built on SDL. Making use of SDL_ttf and IMGUI.
    JPSResearch will be a IGameApplication.

---
+++ JPSResearch/JPSApp.cpp
@@ -0,0 +1,21 @@
+#include "JPSApp.h"
+
+#include "SceneManager.h"
+#include "Scene.h"
+#include "GameObject.h"
+#include "ResourceManager.h"
+
+namespace jpsResearch
+{
+    void JPSApp::Load()
+    {
+    }
+
+    void JPSApp::Update(float)
+    {
+    }
+
+    void JPSApp::FixedUpdate(float)
+    {
+    }
+}
---
+++ JPSResearch/JPSApp.h
@@ -0,0 +1,22 @@
+#pragma once
+#include "IGameApplication.h"
+
+namespace jpsResearch
+{
+    class JPSApp final : public ge::IGameApplication
+    {
+    public:
+        JPSApp() = default;
+        ~JPSApp() override = default;
+        JPSApp(const JPSApp&) = delete;
+        JPSApp(JPSApp&&) = delete;
+        JPSApp& operator=(const JPSApp&) = delete;
+        JPSApp& operator=(JPSApp&&) = delete;
+
+        void Load() override;
+        void Update(float deltaTime) override;
+        void FixedUpdate(float fixedDelta) override;
+
+    private:
+    };
+}
